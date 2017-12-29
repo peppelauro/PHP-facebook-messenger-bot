879d6d49b9bc20aba21a99a7795d36715f61fd25
peppelauro <peppe.lauro@gmail.com>
2017-12-29 18:57:32 +0100

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -2,17 +2,65 @@
 require_once 'config.php';
 require_once 'FacebookBot.php';
 $bot = new FacebookBot(FACEBOOK_VALIDATION_TOKEN, FACEBOOK_PAGE_ACCESS_TOKEN);
+$bot->setWelcomeMessage(FACEBOOK_PAGE_ID,"Hello {{user_first_name}}!\n\nUn bot di test");
+$bot->setGetStartedButton(FACEBOOK_PAGE_ID,'PAYLOAD_START');
+//$bot->setPersistentMenu();
 $bot->run();
 $messages = $bot->getReceivedMessages();
+$bot->echoLog("\n\n".serialize($messages)."\n\n");
 foreach ($messages as $message)
 {
 	$recipientId = $message->senderId;
 	if($message->text)
 	{
-		$bot->sendTextMessage($recipientId, $message->text);
+		switch($message->text)
+		{
+			case "test quickreply":
+				//DEBUG: $bot->sendTextMessage($recipientId, 'Rispondo con:'.serialize($message));
+				$quickrep1 = new \stdClass();
+				$quickrep1->content_type = 'text';
+				$quickrep1->title = 'TEST1';
+				$quickrep1->payload = 'test1';
+				$quickrep2 = new \stdClass();
+				$quickrep2->content_type = 'text';
+				$quickrep2->title = 'TEST2';
+				$quickrep2->payload = 'test2';
+				$bot->sendTextMessage($recipientId, $message->text,[$quickrep1,$quickrep2]);
+				break;
+			case "test attachment":
+				$att = new \stdClass();
+				$att->type = 'image';
+				$att->payload = new \stdClass();
+				$att->payload->url = 'https://cdn.pixabay.com/photo/2017/12/09/16/41/snow-man-3008179_960_720.jpg';
+				$bot->sendTextMessage($recipientId, null, null, $att);
+				break;
+			default:
+				$bot->setSenderAction($recipientId);
+				$bot->sendTextMessage($recipientId, 'Rispondo con:'.$message->text);
+				break;
+		}
 	}
-	elseif($message->attachments)
+	if($message->attachments)
 	{
 		$bot->sendTextMessage($recipientId, "Attachment received");
+		foreach ($message->attachments as $attachment)
+		{
+			$bot->sendTextMessage($recipientId, 'type: '.$attachment->type);
+			switch($attachment->type)
+			{
+				case 'image':
+					$bot->sendTextMessage($recipientId, 'url: '.$attachment->payload->url);
+					break;
+			}
+			
+		}
+	}
+	if($message->quick_reply)
+	{
+		$bot->sendTextMessage($recipientId, "Postback received by quickrep: ".$message->quick_reply->payload);
+	}
+	if($message->payload)
+	{
+		$bot->sendTextMessage($recipientId, "Postback received: ".$message->payload);
 	}
 }
